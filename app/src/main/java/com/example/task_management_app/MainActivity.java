67797eef614dd8afb3236affd3e64de602428247
Salma22ch <57417762+Salma22ch@users.noreply.github.com>
2020-12-26 12:48:57 +0100

    Add fragment and activities

--- app/src/main/java/com/example/task_management_app/MainActivity.java
+++ app/src/main/java/com/example/task_management_app/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.task_management_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new My_tasks()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener=new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment SelectedFragement=null;
+            switch (item.getItemId())
+            {
+                case R.id.Tasks:
+                    SelectedFragement=new My_tasks();
+                    break;
+                case R.id.Calendar:
+                    SelectedFragement=new Calendar();
+                    break;
+                case R.id.Goals:
+                    SelectedFragement=new Goals();
+                    break;
+                case R.id.Completed:
+                    SelectedFragement=new Completed();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,SelectedFragement).commit();
+            return true;
+        }
+    };
+
 }
---
+++ app/src/main/java/com/example/task_management_app/My_tasks.java
@@ -0,0 +1,24 @@
+package com.example.task_management_app;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.zip.Inflater;
+
+public class My_tasks extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mytask, container, false);
+
+    }
+}
+
