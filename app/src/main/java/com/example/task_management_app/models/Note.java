96de03638f7ef233c4f612488f3199cb545cb2ac
Abderrahim Tantaoui <abdoutanta@gmail.com>
2020-12-27 16:30:11 +0100

    added the Note Class Model

---
+++ app/src/main/java/com/example/task_management_app/models/Note.java
@@ -0,0 +1,88 @@
+package com.example.task_management_app.models;
+
+import java.util.Date;
+
+public class Note {
+
+    // Attributes
+    private Integer id;
+    private String title;
+    private String description;
+    private String category;
+    private Date date;
+    private Integer priority;
+    private String state;
+
+    // Constructors
+
+    public Note(Integer id, String title, String description, String category, Date date, Integer priority, String state) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.date = date;
+        this.priority = priority;
+        this.state = state;
+    }
+
+    public Note() {}
+
+    // Getters and Setters
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
