947492bfaf004b71fe36c79da2f6e96cc0f4df1d
Salma22ch <57417762+Salma22ch@users.noreply.github.com>
2020-12-30 23:27:47 +0100

    add category picker dialog -- third version

--- app/src/main/java/com/example/task_management_app/Add/Add.java
+++ app/src/main/java/com/example/task_management_app/Add/Add.java
@@ -1,6 +1,8 @@
 package com.example.task_management_app.Add;
 
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.LayoutInflater;
@@ -14,6 +16,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
@@ -22,6 +25,8 @@ import android.app.TimePickerDialog;
 import java.util.Calendar;
 import android.app.DatePickerDialog;
 import com.example.task_management_app.R;
+import com.google.android.material.timepicker.TimeFormat;
+
 import android.text.format.DateFormat;
 import android.widget.TimePicker;
 
@@ -39,6 +44,9 @@ public class Add extends  DialogFragment implements View.OnClickListener {
     TimePickerDialog time_picker;
     EditText text_time;
     String db_time;
+
+    //category_picker
+    String db_category;
     public static Add newInstance() {
         return new Add();
     }
@@ -62,10 +70,14 @@ public class Add extends  DialogFragment implements View.OnClickListener {
         TextView action = view.findViewById(R.id.fullscreen_dialog_action);
         ImageButton btnday = view.findViewById(R.id.DayButton);
         ImageButton btntime = view.findViewById(R.id.TimeButton);
+        ImageButton btncateg = view.findViewById(R.id.CategoryButton);
+        ImageButton btnnot= view.findViewById(R.id.NotifButton);
         close.setOnClickListener(this);
         action.setOnClickListener(this);
         btnday.setOnClickListener(this);
-        btntime .setOnClickListener(this);
+        btntime.setOnClickListener(this);
+        btncateg.setOnClickListener(this);
+        btnnot.setOnClickListener(this);
         return view;
     }
 
@@ -85,7 +97,11 @@ public class Add extends  DialogFragment implements View.OnClickListener {
                 break;
 
             case R.id.DayButton:
-            { final Calendar c = Calendar.getInstance();
+            {
+                ImageButton btnday= v.findViewById(R.id.DayButton);
+                btnday.setBackgroundResource(R.drawable.background_blue_light);
+                btnday.setColorFilter(Color.argb(255, 255, 255, 255));
+                final Calendar c = Calendar.getInstance();
                 int year = c.get(Calendar.YEAR );
                 int month = c.get(Calendar.MONTH);
                 int day = c.get(Calendar.DAY_OF_MONTH);
@@ -107,7 +123,9 @@ public class Add extends  DialogFragment implements View.OnClickListener {
                 break;}
             case R.id.TimeButton:
             {
-
+                ImageButton btntime = v.findViewById(R.id.TimeButton);
+                btntime.setBackgroundResource(R.drawable.background_blue_light);
+                btntime.setColorFilter(Color.argb(255, 255, 255, 255));
                 Calendar c = Calendar.getInstance();
                 int hour = c.get(Calendar.HOUR_OF_DAY);
                 int minute = c.get(Calendar.MINUTE);
@@ -118,12 +136,67 @@ public class Add extends  DialogFragment implements View.OnClickListener {
                                Calendar c = Calendar.getInstance();
                                c.set(Calendar.HOUR_OF_DAY,sHour);
                                c.set(Calendar.MINUTE ,sMinute);
-                               db_time="Selected Date: "+ sHour +":"+ sMinute ;
+                               db_time=String.format("%02d:%02d", sHour, sMinute);
                                System.out.println(db_time);
+                               //String currentDateString = TimeFormat.getTimeInstance(DateFormat.FULL).format(c.getTime());
                            }
                        }, hour, minute, true);
                 time_picker.show();
                 break;
+            }
+            case R.id.CategoryButton:
+            {
+                ImageButton btncateg = v.findViewById(R.id.CategoryButton);
+                btncateg.setBackgroundResource(R.drawable.background_blue_light);
+                btncateg.setColorFilter(Color.argb(255, 255, 255, 255));
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Choose a category");
+                final String[] list_category = {"Work", "Health", "Home", "Sport" ,"Others"};
+                builder.setSingleChoiceItems(list_category, 0, null).setPositiveButton("Ok",
+                        new DialogInterface.OnClickListener(){
+                    @Override
+                public void onClick(DialogInterface dialog, int selectedPosition) {
+                        dialog.dismiss();
+                        int which = ((AlertDialog)dialog).getListView().getCheckedItemPosition();
+                        switch (which) {
+                            case 0:
+                                db_category="Work";
+                                break;
+
+                            case 1:
+                                db_category="Health";
+                                break;
+
+                            case 2:
+                                db_category="Home";
+                                break;
+
+                            case 3:
+                                db_category="Sport";
+                                break;
+
+                            case 4:
+                                db_category="Others";
+                                break;
+
+                        }
+                        System.out.println(db_category);
+
+                }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.show();
+                break;
+            }
+
+            case R.id.NotifButton:
+            {
+                ImageButton btnnot = v.findViewById(R.id.NotifButton);
+                btnnot.setBackgroundResource(R.drawable.background_blue_light);
+                btnnot.setColorFilter(Color.argb(255, 255, 255, 255));
             }
 
         }
