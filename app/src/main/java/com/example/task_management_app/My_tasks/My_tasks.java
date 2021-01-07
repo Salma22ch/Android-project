17eaeb66242af26187d3fa95aec6001e5797d535
Abderrahim Tantaoui <abdoutanta@gmail.com>
2021-01-07 19:35:31 +0100

    List Recycler Adapter first version

--- app/src/main/java/com/example/task_management_app/My_tasks/My_tasks.java
+++ app/src/main/java/com/example/task_management_app/My_tasks/My_tasks.java
@@ -9,17 +9,45 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.task_management_app.R;
+import com.example.task_management_app.adapters.TaskRecyclerAdapter;
+import com.example.task_management_app.models.Note;
 
+import java.util.ArrayList;
 import java.util.zip.Inflater;
 
 public class My_tasks extends Fragment{
 
+    private RecyclerView recyclerView;
+    private TaskRecyclerAdapter taskRecyclerAdapter;
+    private ArrayList<Note> notes = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mytasks, container, false);
+
+        //inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_mytasks, container, false);
+
+        //creating the recyclerview
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        taskRecyclerAdapter = new TaskRecyclerAdapter(notes);
+        recyclerView.setAdapter(taskRecyclerAdapter);
+
+
+        //populate the list with fake tasks
+        for(int i = 0; i < 1000; i++){
+            Note note = new Note();
+            note.setTitle("title #" + i);
+            notes.add(note);
+        }
+        taskRecyclerAdapter.notifyDataSetChanged();
+
+        return view;
 
     }
 }
---
+++ app/src/main/java/com/example/task_management_app/adapters/TaskRecyclerAdapter.java
@@ -0,0 +1,53 @@
+package com.example.task_management_app.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.task_management_app.R;
+import com.example.task_management_app.models.Note;
+
+import java.util.ArrayList;
+
+public class TaskRecyclerAdapter extends RecyclerView.Adapter<TaskRecyclerAdapter.ViewHolder> {
+
+    private ArrayList<Note> mNotes = new ArrayList<>();
+
+    public TaskRecyclerAdapter(ArrayList<Note> mNotes) {
+        this.mNotes = mNotes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_task, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+
+        holder.title.setText(mNotes.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNotes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView timestamp, title;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.note_title);
+
+        }
+    }
+
+}
