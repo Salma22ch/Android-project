1503c0116c1e4247adae9b28795231dfb49d007e
Abderrahim Tantaoui <abdoutanta@gmail.com>
2021-01-09 15:29:30 +0100

    fetching task list name from database

--- app/src/main/java/com/example/task_management_app/My_tasks/My_tasks.java
+++ app/src/main/java/com/example/task_management_app/My_tasks/My_tasks.java
@@ -1,7 +1,13 @@
 package com.example.task_management_app.My_tasks;
 
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,20 +20,28 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.task_management_app.R;
 import com.example.task_management_app.adapters.TaskRecyclerAdapter;
+import com.example.task_management_app.models.DBOpenHelper;
 import com.example.task_management_app.models.Note;
 
 import java.util.ArrayList;
-import java.util.zip.Inflater;
 
 public class My_tasks extends Fragment{
 
+    private SQLiteDatabase db;
+    private DBOpenHelper dbHelper;
     private RecyclerView recyclerView;
     private TaskRecyclerAdapter taskRecyclerAdapter;
-    private ArrayList<Note> notes = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        // Create or retrieve the database
+        dbHelper = new DBOpenHelper(this.getContext(), DBOpenHelper.Constants.DATABASE_NAME, null,
+                DBOpenHelper.Constants.DATABASE_VERSION);
+        // open the database
+        open();
 
         //inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_mytasks, container, false);
@@ -35,20 +49,50 @@ public class My_tasks extends Fragment{
         //creating the recyclerview
         recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
-        taskRecyclerAdapter = new TaskRecyclerAdapter(notes);
+        taskRecyclerAdapter = new TaskRecyclerAdapter(getDatabaseTasks());
         recyclerView.setAdapter(taskRecyclerAdapter);
-
-
-        //populate the list with fake tasks
-        for(int i = 0; i < 1000; i++){
-            Note note = new Note();
-            note.setTitle("title #" + i);
-            notes.add(note);
-        }
         taskRecyclerAdapter.notifyDataSetChanged();
 
         return view;
 
     }
+
+    public void open() throws SQLiteException {
+        try {
+            db = dbHelper.getWritableDatabase();
+        } catch (SQLiteException ex) {
+            db = dbHelper.getReadableDatabase();
+        }
+    }
+
+    public void close() {
+        db.close();
+    }
+
+    public ArrayList<Note> getDatabaseTasks() {
+        ArrayList<Note> notes = new ArrayList<Note>();
+        Note note;
+        Cursor res = db.rawQuery("select * from Note", null);
+        res.moveToFirst();
+
+        while (res.isAfterLast() == false) {
+            Integer id = res.getInt(res.getColumnIndex(DBOpenHelper.Constants.KEY_COL_ID));
+            String title = res.getString(res.getColumnIndex(DBOpenHelper.Constants.KEY_COL_TITLE));
+            String description = res.getString(res.getColumnIndex(DBOpenHelper.Constants.KEY_COL_DESCRIPTION));
+            String category = res.getString(res.getColumnIndex(DBOpenHelper.Constants.KEY_COL_CATEGORY));
+            long date = res.getLong(res.getColumnIndex(DBOpenHelper.Constants.KEY_COL_DATE));
+            String priority = res.getString(res.getColumnIndex(DBOpenHelper.Constants.KEY_COL_PRIORITY));
+            String state = res.getString(res.getColumnIndex(DBOpenHelper.Constants.KEY_COL_STATE));
+            String type = res.getString(res.getColumnIndex(DBOpenHelper.Constants.KEY_COL_TYPE));
+
+            note = new Note(id, title, description, category, date, priority, state, type);
+            notes.add(note);
+
+            res.moveToNext();
+        }
+        return notes;
+    }
+
+
 }
 
